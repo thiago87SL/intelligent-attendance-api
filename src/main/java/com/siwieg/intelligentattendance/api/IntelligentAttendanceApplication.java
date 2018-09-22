232101d84e28501f13afd662cdab1d33f0c5a988
thiago leite <thiagoleite.ep@gmail.com>
2018-09-22 14:06:37 +0100

    Initial commit for the project

---
+++ src/main/java/com/siwieg/intelligentattendance/api/IntelligentAttendanceApplication.java
@@ -0,0 +1,12 @@
+package com.siwieg.intelligentattendance.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentAttendanceApplication.class, args);
+	}
+}
