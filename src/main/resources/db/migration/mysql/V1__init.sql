0dd7ad3d09e284a5fc6160ac53683ee891e5d9be
thiago leite <thiagoleite.ep@gmail.com>
2018-10-20 18:54:22 +0100

    fixing settings

--- src/main/resources/db/migration/mysql/V1__init.sql
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -1,69 +1,84 @@
-
--- *************************************************************************** --
--- VERSION 1.0.0 (September-23-2018) - Initial Project Development             --
--- *************************************************************************** --
-
---
--- TABLES' STRUCTURES
---
-
 CREATE TABLE `empresa` (
-	`id` bigint(20) NOT NULL,
-	`cnpj` varchar(255) NOT NULL,
-	`data_atualizacao` datetime NOT NULL,
-	`data_criacao` datetime NOT NULL,
-	`razao_social` varchar(255) NOT NULL
+  `id` bigint(20) NOT NULL,
+  `cnpj` varchar(255) NOT NULL,
+  `data_atualizacao` datetime NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `razao_social` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `funcionario` (
-	`id` bigint(20) NOT NULL,
-	`cpf` varchar(255) NOT NULL,
-	`data_atualizacao` datetime NOT NULL,
-	`data_criacao` datetime NOT NULL,
-	`email` varchar(255) NOT NULL,
-	`nome` varchar(255) NOT NULL,
-	`perfil` varchar(255) NOT NULL,
-	`qtd_horas_almoco` float DEFAULT NULL,
-	`qtd_horas_trabalho_dia` float DEFAULT NULL,
-	`senha` varchar(255) NOT NULL,
-	`valor_hora` decimal(19,2) DEFAULT NULL,
-	`empresa_id` bigint(20) DEFAULT NULL
+  `id` bigint(20) NOT NULL,
+  `cpf` varchar(255) NOT NULL,
+  `data_atualizacao` datetime NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `perfil` varchar(255) NOT NULL,
+  `qtd_horas_almoco` float DEFAULT NULL,
+  `qtd_horas_trabalho_dia` float DEFAULT NULL,
+  `senha` varchar(255) NOT NULL,
+  `valor_hora` decimal(19,2) DEFAULT NULL,
+  `empresa_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `lancamento` (
-	`id` bigint(20) NOT NULL,
-	`data` datetime NOT NULL,
-	`data_atualizacao` datetime NOT NULL,
-	`data_criacao` datetime NOT NULL,
-	`descricao` varchar(255) DEFAULT NULL,
-	`localizacao` varchar(255) DEFAULT NULL,
-	`tipo` varchar(255) NOT NULL,
-	`funcionario_id` bigint(20) DEFAULT NULL	
+  `id` bigint(20) NOT NULL,
+  `data` datetime NOT NULL,
+  `data_atualizacao` datetime NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `localizacao` varchar(255) DEFAULT NULL,
+  `tipo` varchar(255) NOT NULL,
+  `funcionario_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- PRIMARY KEYS AND INDEXES FOR TABLES
+-- Indexes for table `empresa`
+--
+ALTER TABLE `empresa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK4cm1kg523jlopyexjbmi6y54j` (`empresa_id`);
+
+--
+-- Indexes for table `lancamento`
+--
+ALTER TABLE `lancamento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK46i4k5vl8wah7feutye9kbpi4` (`funcionario_id`);
+
+--
+-- AUTO_INCREMENT for table `empresa`
+--
+ALTER TABLE `empresa`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `lancamento`
+--
+ALTER TABLE `lancamento`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
 --
 
-ALTER TABLE `empresa` ADD PRIMARY KEY (`id`);
-ALTER TABLE `funcionario` ADD PRIMARY KEY (`id`), ADD KEY `FK_empresa` (`empresa_id`);
-ALTER TABLE `lancamento` ADD PRIMARY KEY (`id`), ADD KEY `FK_funcionario` (`funcionario_id`);
-
--- 
--- AUTO_INCREMENT FOR TABLES
--- 
-
-ALTER TABLE `empresa` MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `funcionario` MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `lancamento` MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-
--- 
--- CONSTRAINTS FOR TABLES
 --
- 
-ALTER TABLE `funcionario` ADD CONSTRAINT `FK3ohhd4i7v1` FOREIGN KEY (`empresa_id`) REFERENCES `empresa` (`id`);
-ALTER TABLE `lancamento` ADD CONSTRAINT `FKc3wgez3670` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionario` (`id`);
- 
--- *************************************************************************** --
--- END of VERSION 1.0.0 (September-23-2018)                                    --
--- *************************************************************************** --
+-- Constraints for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD CONSTRAINT `FK4cm1kg523jlopyexjbmi6y54j` FOREIGN KEY (`empresa_id`) REFERENCES `empresa` (`id`);
+
+--
+-- Constraints for table `lancamento`
+--
+ALTER TABLE `lancamento`
+  ADD CONSTRAINT `FK46i4k5vl8wah7feutye9kbpi4` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionario` (`id`);
